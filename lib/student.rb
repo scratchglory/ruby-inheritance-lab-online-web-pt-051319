69583dd9023b14f680738843d9156b414c375ef3
Adella <adellakenn@gmail.com>
2019-06-13 18:23:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
+=begin
+  Student
+  inherits from the User class
+  #first_name
+    has a first name (FAILED - 1)
+  #last_name
+    has a last name (FAILED - 2)
+  #new
+    initializes with an empty knowledge array (FAILED - 3)
+  #learn
+    takes in an argument of a string of knowledge and adds it to the student's knowledge array (FAILED - 4)
+  #knowledge
+    returns that student's knowledge array (FAILED - 5)
+=end
 
 class Student < User
   def first_name
