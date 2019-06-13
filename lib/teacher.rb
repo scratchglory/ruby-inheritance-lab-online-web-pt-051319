699eb721887d5660a917664323a04cc9fc0dafbf
Adella <adellakenn@gmail.com>
2019-06-13 18:23:39 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,3 +1,14 @@
+=begin
+  Teacher
+  inherits from the User class (FAILED - 6)
+  #first_name
+    has a first name (FAILED - 7)
+  #last_name
+    has a last name (FAILED - 8)
+  #teach
+    returns a random string of knowledge (FAILED - 9)
+=end
+
 class Teacher
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
