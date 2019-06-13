6393575b566a87ae94a742636d1cd3d0056773f5
Adella <adellakenn@gmail.com>
2019-06-13 18:30:57 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -14,7 +14,7 @@ class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  def teach 
+  def teach           # returns a random string of knowledge
     # binding.pry
     KNOWLEDGE.sample
   end
